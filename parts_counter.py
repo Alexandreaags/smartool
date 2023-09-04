8f3918f8a04299830cd6f908b0dc96671294e1c4
“Tassio <tassio-neves@hotmail.com.br>
2023-09-04 14:53:03 +0200

    added plot to parts_counter

--- parts_counter.py
+++ parts_counter.py
@@ -1,5 +1,5 @@
 import serial
-
+import matplotlib.pyplot as plt
 
 ser = serial.Serial('COM13', 115200) # <------ remember to change the port according to yours
 
@@ -25,10 +25,12 @@ try:
     acc = acc_reader()
     part_count = 0
     part_detected = False #Flag to count the part only one time
+    data_acc = []
+    x = []
 
     while True:
         print("Running...")
-        
+        data_acc.append(acc.read_acceleration())
         data = acc.read_acceleration()
         if data > accel_threshold and not part_detected:
             print("Changing of position detected in X axis!")
@@ -40,4 +42,11 @@ try:
 
 except KeyboardInterrupt:
     ser.close()
+    for i in range(0, len(data_acc)):
+            x.append(i)
     print("Amount of manufactured parts:", part_count/2)
+
+    plt.plot(x, data_acc)
+    plt.xlabel('Samples')
+    plt.ylabel('m/s')
+    plt.show()
