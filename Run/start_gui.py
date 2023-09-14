c8027ee9a3acafe3d29e5537e3f8a7bf8086c55d
“Tassio <tassio-neves@hotmail.com.br>
2023-09-14 16:14:05 +0200

    Added "start_gui.py" and "experiment.yml"

---
+++ Run/start_gui.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QApplication
+
+from Smartool.Model.main_model import ArduinoNano
+from Smartool.View.main_window import MainWindow
+
+experiment = ArduinoNano('Run\experiment.yml')
+experiment.load_config()
+experiment.load_daq()
+
+app = QApplication([])
+window = MainWindow(experiment)
+window.show()
+app.exec()
+
+experiment.finalize()
