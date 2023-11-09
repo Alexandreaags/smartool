b4f6f247ab2dc534103d378b59eadea0121f8673
“Tassio <tassio-neves@hotmail.com.br>
2023-11-09 16:28:24 +0100

    arduino file corrected

--- Arduino_Files/arduino_nano_smartool_database.cpp
+++ Arduino_Files/arduino_nano_smartool_database.cpp
@@ -67,7 +67,7 @@ int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "172.18.1.131";    // IPv4 ADRESS FOR NOTEBOOK WIFI NETWORK, NOT THE WIFI ROUTER!!!
+char server[] = "192.168.137.1";    // IPv4 ADRESS FOR NOTEBOOK WIFI NETWORK, NOT THE WIFI ROUTER!!!
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -249,7 +249,7 @@ void loop() {
     client.println("Connection: close");
     client.println();
     /////////////////////////
-    delay(2000);
+    delay(100);
   }
 }
 
