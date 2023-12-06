e5fc8a04b401c175d6fe2b0d68bf080077a911e5
alexandreaags <alexandre.adonai2002@hotmail.com>
2023-12-06 17:28:02 +0100

    add db reading

--- parts_counter.py
+++ parts_counter.py
@@ -7,45 +7,48 @@ import pandas as pd
 #ser = serial.Serial('COM13', 115200) # <------ remember to change the port according to yours
 sqlEngine       = create_engine('mysql+pymysql://ipk:fraunhoferipk@192.168.137.1', pool_recycle=3600)
 dbConnection    = sqlEngine.connect()
-frame           = pd.read_sql("select ID, data_4 from arduino.sql_data WHERE ID > 2230 ", dbConnection)
+frame           = pd.read_sql("select ID, data_1 from arduino.sql_data WHERE ID > 0 ", dbConnection)
 
 accel_threshold = 1 # Threshold of the acceleration 
 
 
 class acc_reader():
 
-    def get_serial_message(self):
-        #ser.flushInput()
-        line = ser.readline()  # Recebe os bytes diretamente
-        values = line.decode('latin-1').strip().split()
-        if len(values) > 0 and values[0] == 'A':
-            return values
-        else:
-            return self.get_serial_message()
-
     def read_acceleration(self):
-        self.x_accel = []
-        while True:
-            line = ser.readline()  # Recebe os bytes diretamente
-            try:
-                values = line.decode('latin-1').strip().split() #strip remove blank spaces
-                if len(values) == 3:
-                    self.x_accel = float(values[0])
-                    return self.x_accel + 9.58
-            except UnicodeDecodeError:
-                pass  # Ignora os bytes que não podem ser decodificados
+        return frame['data_1'] - 0.35
+    # def get_serial_message(self):
+    #     #ser.flushInput()
+    #     #line = ser.readline()  # Recebe os bytes diretamente
+    #     values = line.decode('latin-1').strip().split()
+    #     if len(values) > 0 and values[0] == 'A':
+    #         return values
+    #     else:
+    #         return self.get_serial_message()
+
+    # def read_acceleration(self):
+    #     self.x_accel = []
+    #     while True:
+    #         line = ser.readline()  # Recebe os bytes diretamente
+    #         try:
+    #             values = line.decode('latin-1').strip().split() #strip remove blank spaces
+    #             if len(values) == 3:
+    #                 self.x_accel = float(values[0])
+    #                 return self.x_accel + 9.58
+    #         except UnicodeDecodeError:
+    #             pass  # Ignora os bytes que não podem ser decodificados
 
 try:
     acc = acc_reader()
     part_count = 0
     part_detected = False #Flag to count the part only one time
-    # data_acc = []
-    # x = []
+    data_acc = []
+    x = []
     data = acc.read_acceleration()
     print(data)
     for i in data:
         print("Running...")
-        # data_acc.append(acc.read_acceleration())
+        print(i)
+        data_acc.append(i)
         
         if i > accel_threshold and not part_detected:
             print("Changing of position detected in X axis!")
@@ -54,17 +57,19 @@ try:
         
         if i < accel_threshold:
             part_detected = False  # Redefine to False when the position is less than threshold 
-    part_count = part_count/2
-    print(part_count*2)   
+    # part_count = part_count/2
+    print(part_count)   
+    print(data)
+    for i in range(0, len(data_acc)):
+            x.append(i)
+    print("Amount of manufactured parts:", part_count/2)
+
+    plt.plot(x, data_acc)
+    plt.xlabel('Samples')
+    plt.ylabel('m/s')
+    plt.show()
 except KeyboardInterrupt:
     # ser.close()
     
     dbConnection.close()
-    # for i in range(0, len(data_acc)):
-    #         x.append(i)
-    # print("Amount of manufactured parts:", part_count/2)
-
-    # plt.plot(x, data_acc)
-    # plt.xlabel('Samples')
-    # plt.ylabel('m/s')
-    # plt.show()
+    
--- test_db.py
+++ test_db.py
@@ -5,7 +5,7 @@ import pandas as pd
 #ser = serial.Serial('COM13', 115200) # <------ remember to change the port according to yours
 sqlEngine       = create_engine('mysql+pymysql://ipk:fraunhoferipk@192.168.137.1', pool_recycle=3600)
 dbConnection    = sqlEngine.connect()
-frame           = pd.read_sql("select ID, data_4 from arduino.sql_data WHERE ID > 500", dbConnection)
+frame           = pd.read_sql("select ID, data_4 from arduino.sql_data WHERE ID > 0", dbConnection)
 print(frame['data_4'])
 print(type(frame['data_4']))
 accel_threshold = 6 # Threshold of the acceleration 
@@ -15,9 +15,11 @@ class acc_reader():
 
     def read_acceleration(self):
         return frame['data_4'] + 9.58
+
+
     
 acc = acc_reader()
 data = []
 data.append(acc.read_acceleration())
-# print(data)
+print(data)
 # print(type(data))
