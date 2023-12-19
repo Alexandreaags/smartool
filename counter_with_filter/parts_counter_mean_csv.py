5bc0d780f79eaf9842683076d392b2af1f4b6602
alexandreaags <alexandre.adonai2002@hotmail.com>
2023-12-19 16:03:36 +0100

    add counter with mean using csv

--- counter_with_filter/parts_counter_mean_csv.py
+++ counter_with_filter/parts_counter_mean_csv.py
@@ -9,10 +9,10 @@ data_path = {
     'TEST 15 PARTS' : 'C:/Users/Alexa/Programs/Smartool/data/TEST-2-15PARTS-07-12-2023.csv'    
 }
 
-data = pd.read_csv(data_path['TEST 10 PARTS'], sep=";", header=0, decimal=',')
+data = pd.read_csv(data_path['TEST 3 PARTS'], sep=";", header=0, decimal=',')
 
 
-accel_threshold = 2.1 # Threshold of the acceleration 
+accel_threshold = 0.217 # Threshold of the acceleration 
 
 
 
@@ -23,22 +23,32 @@ part_detected = False #Flag to count the part only one time
 # print("Running...")
         
         
-data_acc.append(data['acc_1_x'])
+# data_acc.append(data['acc_1_x'])
+data_acc = data['acc_1_x'].tolist()
+list_data = []
 # print(data_acc)
-last_datas = data_acc[-30:]# separe only the last datas from the list
-sum_data = sum(last_datas)# sum the last 100 datas 
-mean_data = sum_data/len(last_datas)# mean of the last 100 datas
-print(mean_data)
+# x = 0
+x_axis = []
+y_axis = []
+for i in data_acc:
+    # x += 1
+    list_data.append(i)
+    # print(list_data)
+    last_data = list_data[-100:]# separe only the last datas from the list
+    sum_data = sum(last_data)# sum the last 100 datas 
+    mean_data = sum_data/len(last_data)# mean of the last 100 datas
+    x_axis.append(mean_data)
+    # print(f'Mean {x}: {mean_data}')
 
-if mean_data > accel_threshold and not part_detected:
+    if mean_data > accel_threshold and not part_detected:
 
-    print("Changing of position detected in X axis!")
-    part_detected = True
-    part_count += 1  # Incrementa a contagem de peças
-        
-if mean_data < accel_threshold:
-    part_detected = False  # Redefine para Falso quando a posição volta ao normal
+        print("Changing of position detected in X axis!")
+        part_detected = True
+        part_count += 1  # Incrementa a contagem de peças
 
-    print("Amount of manufactured parts:", part_count)
-    # print(data_acc)
+    if mean_data < accel_threshold:
+        part_detected = False  # Redefine para Falso quando a posição volta ao normal
+
+print("Amount of manufactured parts:", part_count)
+        # print(data_acc)
     
