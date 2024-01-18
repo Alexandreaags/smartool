48d96ea9764f2b991e66ec25282fc1280ef2eff7
“Tassio <tassio-neves@hotmail.com.br>
2024-01-18 16:52:56 +0100

    added module 'get_results_cycle'

--- results/operator.py
+++ results/operator.py
@@ -15,7 +15,7 @@ class Operator():
         # Flag for counting
         self.cycle_counter_flag = False
         # Number of samples read in a run
-        self.nr_samples = 500
+        self.nr_samples = 2000
         # Database connection
         self.db_info : {
             'username' : '',
@@ -38,29 +38,45 @@ class Operator():
                                    self.db_info['username'] + ':' + 
                                    self.db_info['password'] + '@' + 
                                    self.db_info['hostname']), pool_recycle=3600)
-        dbConnection    = sqlEngine.connect()
-        self.last_result = pd.read_sql(('SELECT nr_samples_in_mean, sum_mean_ambient_temperature, ' + 
+        dbConnection = sqlEngine.connect()
+        # Send querry
+        self.last_result = pd.read_sql(('SELECT cycle_nr, nr_samples_in_mean, sum_mean_ambient_temperature, ' + 
                                         'sum_mean_ambient_humidity, sum_mean_cavity_temperature, sum_mean_cavity_pressure, ' + 
-                                        'sum_mean_closing_force, last_value_conseq_zero, ' + 
+                                        'sum_mean_closing_force, ' + 
                                         'last_ID FROM arduino.sql_results ORDER BY ID DESC LIMIT 1'), dbConnection)
 
-        self.initial_conseq_zero = self.last_result.iloc[0]['last_value_conseq_zero']
-        self.nr_samples_in_mean = self.last_result.iloc[0]['nr_samples_in_mean']
-        self.data_last_id = int(self.last_result.iloc[0]['last_ID'])
-        self.sum_mean_variables = {
-            'ambient_temperature' : self.last_result.iloc[0]['sum_mean_ambient_temperature'],
-            'ambient_humidity' : self.last_result.iloc[0]['sum_mean_ambient_humidity'],
-            'cavity_temperature' : self.last_result.iloc[0]['sum_mean_cavity_temperature'],
-            'cavity_pressure' : self.last_result.iloc[0]['sum_mean_cavity_pressure'],
-            'closing_force' : self.last_result.iloc[0]['sum_mean_closing_force']
-        }
+        if self.last_result.shape[0] != 0: #sql_results not empty
+            self.last_cycle_db          = int(self.last_result.iloc[0]['cycle_nr'])
+            self.nr_samples_in_mean     = int(self.last_result.iloc[0]['nr_samples_in_mean'])
+            self.data_last_id           = int(self.last_result.iloc[0]['last_ID'])
+            self.sum_mean_variables = {
+                'ambient_temperature'   : self.last_result.iloc[0]['sum_mean_ambient_temperature'],
+                'ambient_humidity'      : self.last_result.iloc[0]['sum_mean_ambient_humidity'],
+                'cavity_temperature'    : self.last_result.iloc[0]['sum_mean_cavity_temperature'],
+                'cavity_pressure'       : self.last_result.iloc[0]['sum_mean_cavity_pressure'],
+                'closing_force'         : self.last_result.iloc[0]['sum_mean_closing_force']
+            }
+        else: #sql_results is empty -> first cycle
+            self.last_cycle_db          = 0
+            self.nr_samples_in_mean     = 0
+            self.data_last_id           = 0
+            self.sum_mean_variables = {
+                'ambient_temperature'   : 0,
+                'ambient_humidity'      : 0,
+                'cavity_temperature'    : 0,
+                'cavity_pressure'       : 0,
+                'closing_force'         : 0
+            }
+
         # Get data
         self.data = pd.read_sql(('SELECT * FROM arduino.sql_data WHERE ID > ' + str(self.data_last_id) + 
                                 ' LIMIT ' + str(self.nr_samples)), dbConnection)
         self.data.columns = ['ID', 'acc_1_x', 'acc_1_y', 'acc_1_z', 'acc_2_x', 'acc_2_y', 
                              'acc_2_z', 'lastTemp', 'lastHum', 'tempKistler1', 'data_10', 'date']
 
-        print(self.data)
+        dbConnection.close()
+
+        #print(self.data)
 
         # Consecutives zeroes array
         self.conseq_zero = np.zeros(len(self.data))
@@ -68,8 +84,6 @@ class Operator():
         self.spaces = np.zeros(len(self.data))
         # Parts counter analyzing rest periods
         self.cycle_counter = np.ones(len(self.data))
-
-        dbConnection.close()
 
     def get_data_csv(self):
         # Invert Index
@@ -115,20 +129,74 @@ class Operator():
         self.flag = True
         #Define the cycle for every row in Dataframe 
         for i in range(0, len(self.spaces)):
-            if self.spaces[i] == 0 and flag == False:
+            if self.spaces[i] == 0 and self.flag == False:
                 counter += 1
                 self.cycle_counter[i] = counter
-                flag = True
+                self.flag = True
             elif self.spaces[i] == 1:
                 self.cycle_counter[i] = counter
-                flag = False
+                self.flag = False
     
     def flag_rest(self):
         #Add 'cycle_nr' and 'is_resting' arrays to Results Dataframe 
         self.data_rest_flagged = self.data
         self.data_rest_flagged.insert(1, "is_resting", self.spaces)
         self.data_rest_flagged.insert(2, "cycle_nr", self.cycle_counter)
-    
+        
+    def get_results_cycle(self):
+        #Creating Results Dataframe to be further added in Database
+        self.results = pd.DataFrame(columns=['cycle_nr', 'cycle_ambient_temperature', 'cycle_ambient_humidity', 
+                                             'cycle_cavity_temperature', 'cycle_cavity_pressure', 'cycle_closing_force', 'last_ID'])
+        #cycle_ambient_temperature  -- all the time
+        #cycle_ambient_humidity     -- all the time
+        #cycle_cavity_temperature   -- while closed
+        #cycle_cavity_pressure      -- while closed
+        #cycle_closing_force        -- while closed
+
+        self.cycle_ambient_temperature = 0
+        self.cycle_ambient_humidity = 0
+        self.cycle_cavity_temperature = 0
+        #self.cycle_cavity_pressure = 0
+        #self.cycle_closing_force = 0
+
+        #self.mean_ambient_temperature = 0
+        #self.mean_ambient_humidity = 0
+        #self.mean_cavity_temperature = 0
+        #self.mean_cavity_pressure = 0
+        #self.mean_closing_force = 0
+
+        #Define actual cycle
+        cycle = self.last_cycle_db + 1
+
+        #Calculate mean for all variables
+        array_ambient_temperature = self.data_rest_flagged.loc[self.data_rest_flagged['cycle_nr'] == 0].loc[:, 'lastTemp']
+        if len(array_ambient_temperature) == 0:
+            array_ambient_temperature = [0]
+        array_ambient_humidity = self.data_rest_flagged.loc[self.data_rest_flagged['cycle_nr'] == 0].loc[:, 'lastHum']
+        if len(array_ambient_humidity) == 0:
+            array_ambient_humidity = [0]
+        array_cavity_temperature = self.data_rest_flagged.loc[(self.data_rest_flagged['cycle_nr'] == 0) & (self.data_rest_flagged['is_resting'] == 0)].loc[:, 'tempKistler1']
+        if len(array_cavity_temperature) == 0:
+            array_cavity_temperature = [0]
+        print(self.data_rest_flagged.loc[self.data_rest_flagged['cycle_nr'] == 0].loc[:, 'ID'])
+        last_ID = int(self.data_rest_flagged.loc[self.data_rest_flagged['cycle_nr'] == 0].loc[:, 'ID'].iloc[-1])
+        print(last_ID)
+        
+        #array_cavity_pressure = self.data_rest_flagged.loc[[(self.data_rest_flagged['cycle_nr'] == cycle) & (self.data_rest_flagged['is_resting'] == 0)], ['']]
+        #array_closing_force = self.data_rest_flagged.loc[[(self.data_rest_flagged['cycle_nr'] == cycle) & (self.data_rest_flagged['is_resting'] == 0)], ['']]
+        
+        #Define a dictionary with the values for a cycle
+        new_row = {'cycle_nr'                   : cycle, 
+                   'cycle_ambient_temperature'  : mean(array_ambient_temperature),
+                   'cycle_ambient_humidity'     : mean(array_ambient_humidity),
+                   'cycle_cavity_temperature'   : mean(array_cavity_temperature),
+                   'cycle_cavity_pressure'      : 0,
+                   'cycle_closing_force'        : 0,
+                   'last_ID'                    : last_ID}
+        
+        #Insert dictionary in Dataframe
+        self.results.loc[len(self.results)] = new_row
+
     def get_results(self):
         #Creating Results Dataframe to be further added in Database
         self.results = pd.DataFrame(columns=['cycle_nr', 'cycle_ambient_temperature', 'cycle_ambient_humidity', 
@@ -184,8 +252,8 @@ class Operator():
 
         print(self.results) 
     
-    def insert_in_db(self, username, password, hostname):
-        engine_config = 'mysql+pymysql://' + username + ':' + password + '@' + hostname
+    def insert_in_db(self):
+        engine_config = 'mysql+pymysql://' + self.db_info['username'] + ':' + self.db_info['password'] + '@' + self.db_info['hostname']
         engine = create_engine(engine_config, pool_recycle=3600)
         dbConnection = engine.connect()
         # if_exists='replace' will replace the table if it already exists. You can change it to 'append' if you want to add rows to an existing table.
@@ -215,15 +283,14 @@ op.flag_rest()
 
 #print(op.data_rest_flagged)
 
-op.get_results()
-#op.insert_in_db('root', 'tassio25789', '127.0.0.1')
-#op.insert_in_db('root', '', '127.0.0.1')
-
+op.get_results_cycle()
+#print(op.results)
+op.insert_in_db()
 
 # PLOT SPACES
 plt.plot(op.data.iloc[:]['ID'].to_numpy(), op.spaces, 'c')
 # PLOT FILTERED DATA
-#plt.plot(op.data.iloc[:]['ID'].to_numpy(), op.data.iloc[:]['acc_1_x'].to_numpy(), 'r')
+plt.plot(op.data.iloc[:]['ID'].to_numpy(), op.data.iloc[:]['acc_1_x'].to_numpy(), 'r')
 # PLOT ZEROS COUNTER
 #plt.plot(op.data.iloc[:]['ID'].to_numpy(), op.conseq_zero, 'b', lw = 0.2)
 
